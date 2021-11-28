3e14320553838b6c28a057d053449e76a248b5fe
hung2504 <vanhung25042000@gmail.com>
2021-11-28 22:45:43 +0700

    Finish Retry (Fallback)

--- src/main/java/com/example/customerservice/service/CustomerService.java
+++ src/main/java/com/example/customerservice/service/CustomerService.java
@@ -2,6 +2,7 @@ package com.example.customerservice.service;
 
 import com.example.customerservice.entity.Customer;
 import com.example.customerservice.repository.CustomerRepository;
+import io.github.resilience4j.retry.annotation.Retry;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cache.annotation.CacheEvict;
 import org.springframework.cache.annotation.Cacheable;
@@ -17,8 +18,14 @@ public class CustomerService {
         return customerRepository.save(customer);
     }
 
+
+    @Retry(name = "retryService", fallbackMethod = "localCacheCustomerSearch")
     public Customer findCustomerById(Long id){
         System.out.println("Find by id from DB");
+        return customerRepository.findById(id).get();
+    }
+    private Customer localCacheCustomerSearch(Long id, RuntimeException re) {
+        System.out.println("Returning search results from cache");
         return customerRepository.findById(id).get();
     }
 
